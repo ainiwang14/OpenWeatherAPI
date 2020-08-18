279c78bfbad2b240b5475244d1577b08597bfa01
AileenWang <ainiwang14@gmail.com>
2020-08-17 17:23:40 -0700

    -m

--- stuff.js
+++ stuff.js
@@ -1,55 +1,59 @@
 $(document).ready(function () {
     //name const
 
-    //function to grab weather api
-    // function getWeatherData() {
-    //     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=927924e4c73455c7286d71a6b1b45a4c`
+    // function to grab weather api
+    function getWeatherData(geoLocation) {
+        const {lat, lng} = geoLocation;
+        let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=927924e4c73455c7286d71a6b1b45a4c`;
+        let i = 0
         
-    //     $.ajax({
-    //         url,
-    //         method: "GET"
-    //     }).then(response => {
-    //         console.log(response, "weatherData")
-    //     })
-    // }
-
-    // getWeatherData()
+        $.ajax({
+            url,
+            method: "GET"
+        }).then(response => {
+            console.log(response, "weatherData")
+            const uvIndex = response.current.uvi;
+            const temp = response.current.temp;
+            const humidity= response.current.humidity;
+            const forecastTemp= response.daily[i].temp.day;
+            const forecastHumidity= response.daily[i].humidity;
+            const currentWeather = {uvIndex, temp, humidity};
+            const forecastWeather = {forecastTemp, forecastHumidity}
+        
+                //function w/ for loop to make 5 divs and append weather in forecast
+        })
+    }
 
     //fuction to grab uv index -- just kidding the openweather api has uv
 
-    //function w/ for loop to make 5 divs and append weather in forecast
-
     //grab input from submission box
     const btn = $(".btn")
-    
-    function grabInputCity () {
-        btn.click(() => {
-            event.preventDefault();
-            const inputCity = $("#inputCity")[0].value;
-            console.log(inputCity)
-            })
-    }
 
-    grabInputCity()
+    btn.click(() => {
+        event.preventDefault();
+        const inputCity = $("#inputCity")[0].value;
+        cityToGeolocation(inputCity)
+    });
     
     //function to use input from submission to geocode
-    // function cityToGeolocation() {
-    //     let url = `https://api.opencagedata.com/geocode/v1/json?q=${inputCity}&key=6d0e711d72d74daeb2b0bfd2a5cdfdba`
-    //     // don't forget to change api key to f7c536952d884e97baeb7a8948d115a0
+    function cityToGeolocation(inputCity) {
+        let url = `https://api.opencagedata.com/geocode/v1/json?q=${inputCity}&key=f7c536952d884e97baeb7a8948d115a0`
+        // api key for testing: 6d0e711d72d74daeb2b0bfd2a5cdfdba
+        // my api key: f7c536952d884e97baeb7a8948d115a0
 
-    //     $.ajax({
-    //         url,
-    //         method: "GET"
-    //     }).then(response => {
-    //                 console.log(response, "geoLocation")
-    //             })
-    // }
-
-    // cityToGeolocation()
-
-    // function getGeolocation() {
-
-    // }
+        $.ajax({
+            url,
+            method: "GET"
+        }).then(response => {
+            console.log(response, "geoLocation")
+            const lat = response.results[0].geometry.lat;
+            const lng = response.results[0].geometry.lng;
+            console.log(lat)
+            console.log(lng)
+            const geoLocation = {lat, lng};
+            getWeatherData(geoLocation)
+        })
+    }
 
     //function to save to local storage
 
